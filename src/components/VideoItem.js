0d04ca1907941fe6de9a117c70c82360817b63e3
kaisdewani <kaisdewani@yahoo.com>
2019-07-04 13:44:45 -0400

    added classNames for semanticUI to move thumbnails to the left with the title on the right

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 const VideoItem = ({ video }) => {
-  return <div>{video.snippet.title}</div>;
+  return (
+    <div className="item">
+      <img className="ui image" src={video.snippet.thumbnails.medium.url} />
+      <div className="content">
+        <div className="content">{video.snippet.title}</div>
+      </div>
+    </div>
+  );
 };
 
 export default VideoItem;
